9a520eda809b10815e5ba0548c5a0e38e99c58df
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-03-25 10:38:56 +0900

    wip:init App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const App = () => {
-  
-}
+  const [allCountries, setAllCountries] = useState('');
+  return (
+    <div className="App">
+      <Serch />
+      <Countries />
+    </div>
+  );
+};
 
 export default App;
