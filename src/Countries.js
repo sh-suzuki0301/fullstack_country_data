b1c99a761cdbb713bb9ff57438d4ab5389bb133f
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-03-27 08:27:30 +0900

    add show details

--- src/Countries.js
+++ src/Countries.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Countries = ({ countries }) => {
+    const [showDetails, setShowDetails] = useState([]);
+
+    const toggleDetails = id => {
+        let update =[...showDetails];
+        update[id] = !update[id]
+        setShowDetails(update);  
+    };
+
     const renderCountries = countries.map((c, id) => (
-        <div>
-            <Country key={id} country={c} />
+        <div key={c.name}>
+            {c.name} <button onClinck={() => toggleDetails(id)}>show</button>
+            {showDetails[id] && <Country country={c} />}
         </div>
     ));
 
     return (
         <div className="Countries">
-            <div>{renderCountries}</div>
+            {countries.length === 1 ? (
+                <Country country={countries[0]} />
+            ) : (
+                <div>{renderCountries}</div>
+            )}
         </div>
     );
 };
